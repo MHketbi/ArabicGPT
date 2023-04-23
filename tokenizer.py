747e73500b46cb8111f607c55ea6da8bdb21c9db
Mubarak Harran Alketbi <mubarak.harran@gmail.com>
2023-04-23 20:03:53 +0400

    update

--- tokenizer.py
+++ tokenizer.py
@@ -1,21 +1,49 @@
-from tokenizers import Tokenizer, trainers, normalizers, pre_tokenizers, processors
-from tokenizers.models import BPE
+from tokenizers import Tokenizer, models, pre_tokenizers, decoders, normalizers, trainers
+import json
 
-# Create a BPE tokenizer model
-tokenizer = Tokenizer(BPE(unk_token="[UNK]"))
+# Initialize a tokenizer with the BPE model
+tokenizer = Tokenizer(models.BPE())
 
-# Add a normalizer that handles Arabic text
-tokenizer.normalizer = normalizers.Sequence([
-    normalizers.NFKC(),
-    normalizers.Arabic()
-])
+# Set the normalizer
+tokenizer.normalizer = normalizers.NFKC()
 
-# Add a pre-tokenizer that splits the input into words
-tokenizer.pre_tokenizer = pre_tokenizers.Whitespace()
+# Set the pre-tokenizer
+tokenizer.pre_tokenizer = pre_tokenizers.ByteLevel()
 
-# Train the tokenizer on the Arabic corpus
-trainer = trainers.BpeTrainer(vocab_size=30000, min_frequency=2, special_tokens=["[UNK]", "[CLS]", "[SEP]", "[MASK]", "[PAD]"])
-tokenizer.train(files=["arabic_corpus.txt"], trainer=trainer)
+# Set the decoder
+tokenizer.decoder = decoders.ByteLevel()
 
-# Save the tokenizer to a file
-tokenizer.save("MegatronReady.json")
+# Train the tokenizer using the BPE trainer
+trainer = trainers.BpeTrainer(
+    vocab_size=60000,
+    min_frequency=2,
+    special_tokens=["<PAD>", "<BOS>", "<EOS>"],
+)
+
+# Train the tokenizer on your dataset
+files = ["quran-simple-clean.txt"]  # Replace this with the path to your dataset
+tokenizer.train(files, trainer)
+
+# Save the trained tokenizer
+tokenizer.save("arabic_bpe_tokenizer.json")
+
+# Load the trained tokenizer from the saved file
+tokenizer = Tokenizer.from_file("arabic_bpe_tokenizer.json")
+
+# Save the vocab.json and merges.txt files
+with open("vocab.json", "w", encoding="utf-8") as vocab_file, open("merges.txt", "w", encoding="utf-8") as merges_file:
+    vocab = tokenizer.get_vocab()
+    for token, token_id in vocab.items():
+        vocab_file.write(f"{token}\t{token_id}\n")
+
+# Load the tokenizer configuration from the arabic_bpe_tokenizer.json file
+with open("arabic_bpe_tokenizer.json", "r", encoding="utf-8") as config_file:
+    tokenizer_config = json.load(config_file)
+
+# Extract the merges from the tokenizer configuration
+merges = tokenizer_config["model"]["merges"]
+
+# Save the merges to the merges.txt file
+with open("merges.txt", "w", encoding="utf-8") as merges_file:
+    for merge in merges:
+        merges_file.write(f"{merge[0]} {merge[1]}\n")
